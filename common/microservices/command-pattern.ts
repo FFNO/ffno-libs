a066466b4a3cc85fa6bcf93ad9f258cc3d5f1bbd
khang <khang194591@gmail.com>
2024-05-03 16:48:24 +0700

    update

--- common/microservices/command-pattern.ts
+++ common/microservices/command-pattern.ts
@@ -3,4 +3,7 @@ export const NOTIFICATION_PATTERNS = {
   SEND_WEB_PUSH: 'notification.send_web_push',
 };
 
-export const CHAT_PATTERNS = {};
+export const CHAT_PATTERNS = {
+  SEND_MESSAGE: 'chat.send_message',
+  RECEIVE_MESSAGE: 'chat.receive_message',
+};
