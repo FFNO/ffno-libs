0c566276acd7428e002db362d738b7b46b420fc3
Jimmy Nguyen <son.sosc@carehealth.io>
2024-05-09 13:36:24 +0700

    update

--- common/microservices/command-pattern.ts
+++ common/microservices/command-pattern.ts
@@ -10,6 +10,7 @@ export const NOTIFICATION_PATTERNS = {
 export const CHAT_PATTERNS = {
   GET_MESSAGE: 'chat.get-message',
   GET_LIST_MESSAGES: 'chat.get-list-message',
+  GET_LIST_CHANNEL: 'chat.get-list-channel',
   SEND_MESSAGE: 'chat.send_message',
   RECEIVE_MESSAGE: 'chat.receive_message',
 };
