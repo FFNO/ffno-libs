8b1dfe37b92851c05c96aff267d6c8c36e483621
Kyle <khang194591@gmail.com>
2024-05-13 23:27:55 +0700

    update

--- common/enums/invoice.ts
+++ common/enums/invoice.ts
@@ -15,4 +15,13 @@ export const invoiceCategoryRecord: Record<InvoiceCategory, string> = {
   [InvoiceCategory.UNIT_CHARGE]: 'Unit charge',
 };
 
+export const invoiceStatusRecord: Record<InvoiceStatus, string> = {
+  [InvoiceStatus.PENDING]: 'Pending',
+  [InvoiceStatus.PAID]: 'Paid',
+  [InvoiceStatus.PARTIAL]: 'Partial',
+  [InvoiceStatus.OVERDUE]: 'Overdue',
+};
+
 export const invoiceCategories = getOptionListFromRecord(invoiceCategoryRecord);
+
+export const invoiceStatuses = getOptionListFromRecord(invoiceStatusRecord);
