7262c606970fdb6a452cd0a1dc27a96e65641723
Jimmy Nguyen <son.sosc@carehealth.io>
2024-06-06 14:55:46 +0700

    update

--- common/enums/request.ts
+++ common/enums/request.ts
@@ -2,20 +2,24 @@ import { getOptionListFromRecord } from '../../helpers';
 
 export enum RequestCategory {
   UNIT_LEASE = 'UNIT_LEASE',
-  TERMINATE_CONTRACT = 'TERMINATE_CONTRACT',
   REPORT_ISSUE = 'REPORT_ISSUE',
+  REQUEST_EQUIPMENT = 'REQUEST_EQUIPMENT',
   EQUIPMENT_WARRANTY = 'EQUIPMENT_WARRANTY',
+  TERMINATE_CONTRACT = 'TERMINATE_CONTRACT',
 }
 
 export enum RequestStatus {
   PENDING = 'PENDING',
   ACCEPTED = 'ACCEPTED',
   REJECTED = 'REJECTED',
+  HANDLING = 'HANDLING',
+  DONE = 'DONE',
 }
 
 export const requestCategoryRecord: Record<RequestCategory, string> = {
   [RequestCategory.UNIT_LEASE]: 'Unit Lease',
   [RequestCategory.TERMINATE_CONTRACT]: 'Terminate contract',
+  [RequestCategory.REQUEST_EQUIPMENT]: 'Request equipment',
   [RequestCategory.REPORT_ISSUE]: 'Report issue',
   [RequestCategory.EQUIPMENT_WARRANTY]: 'Equipment warranty',
 };
@@ -24,12 +28,16 @@ export const requestStatusRecord: Record<RequestStatus, string> = {
   [RequestStatus.PENDING]: 'Pending',
   [RequestStatus.ACCEPTED]: 'Approved',
   [RequestStatus.REJECTED]: 'Rejected',
+  [RequestStatus.HANDLING]: 'Handling',
+  [RequestStatus.DONE]: 'Done',
 };
 
 export const requestStatusColorRecord = {
   [RequestStatus.ACCEPTED]: 'green',
   [RequestStatus.PENDING]: 'orange',
   [RequestStatus.REJECTED]: 'red',
+  [RequestStatus.HANDLING]: 'orange',
+  [RequestStatus.DONE]: 'green',
 };
 
 export const requestCategories = getOptionListFromRecord(requestCategoryRecord);
