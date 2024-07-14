380bfb0e7e264c4976fd6a1cff459e26e8344e60
khang <khang194591@gmail.com>
2024-07-14 16:54:41 +0700

    update

--- common/enums/invoice.ts
+++ common/enums/invoice.ts
@@ -12,12 +12,16 @@ export enum InvoiceCategory {
   MERGED = 'MERGED',
   UNIT_CHARGE = 'UNIT_CHARGE',
   UNIT_PRICE_LOG = 'UNIT_PRICE_LOG',
+  DEPOSIT_REFUND = 'DEPOSIT_REFUND',
+  DEPOSIT = 'DEPOSIT',
 }
 
 export const invoiceCategoryRecord: Record<InvoiceCategory, string> = {
   [InvoiceCategory.MERGED]: 'Merge',
   [InvoiceCategory.UNIT_CHARGE]: 'Unit charge',
   [InvoiceCategory.UNIT_PRICE_LOG]: 'Unit log price',
+  [InvoiceCategory.DEPOSIT_REFUND]: 'Deposit refund',
+  [InvoiceCategory.DEPOSIT]: 'Deposit',
 };
 
 export const invoiceStatusRecord: Record<InvoiceStatus, string> = {
